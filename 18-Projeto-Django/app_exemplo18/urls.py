ff98569f46bd6d31505c258fb21e92b099893358
Vitor Kruel <vkruel.programador@gmail.com>
2023-08-18 09:42:52 -0300

    Testando a urls home.

---
+++ 18-Projeto-Django/app_exemplo18/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = 'exemplo18'
+
+urlpatterns = [ 
+    path('', lambda: ..., name='home'),
+]
--- 18-Projeto-Django/exemplo_18/urls.py
+++ 18-Projeto-Django/exemplo_18/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('app_exemplo18.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
