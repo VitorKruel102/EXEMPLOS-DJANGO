84e0ebc9e99a07fe809b8cf178147c56e998de9e
Vitor Kruel <vkruel.programador@gmail.com>
2023-08-18 10:16:39 -0300

    Registrando a tabela do banco de dados.

--- 18-Projeto-Django/app_exemplo18/admin.py
+++ 18-Projeto-Django/app_exemplo18/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from app_exemplo18.models import Produtos
 
 # Register your models here.
+@admin.register(Produtos)
+class ProdutosAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'titulo', 'categoria')
