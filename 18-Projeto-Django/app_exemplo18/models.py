3b0a5792dcebf23dce03c740d4a1b5f87c486a92
Vitor Kruel <vkruel.programador@gmail.com>
2023-08-18 10:17:11 -0300

    Criado o models Produtos.

--- 18-Projeto-Django/app_exemplo18/models.py
+++ 18-Projeto-Django/app_exemplo18/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from PIL import Image
+
+class Produtos(models.Model):
+    CATEGORIAS = (
+        ('Roupa M', 'Roupa Masculina'),
+        ('Roupa F', 'Roupa Feminina'),
+        ('Carros', 'Carros'),
+        ('Motos', 'Motos'),
+        ('Casa', 'Casa'),
+    )
+    MAX_SIZE = (250, 250)
+
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField()
+    categoria = models.CharField(max_length=20, choices=CATEGORIAS)
+    imagem_produto = models.ImageField(upload_to='produtos/%Y/%m/%d')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        _imagem = Image.open(self.imagem_produto.path)
+        _imagem.thumbnail(self.MAX_SIZE)
+        _imagem.save(self.imagem_produto.path)
+
+    class Meta:
+        # Defina o nome personalizado da tabela aqui
+        verbose_name_plural = 'Produto'
+
+    def __str__(self):
+        return self.titulo
