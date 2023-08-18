5f3e02b97dca90913cffaa57e3b51e7be80a51d8
Vitor Kruel <vkruel.programador@gmail.com>
2023-08-18 09:50:08 -0300

    Testando se a resposta do cliente é 200

--- 18-Projeto-Django/app_exemplo18/tests/test_exemplo18_home_views.py
+++ 18-Projeto-Django/app_exemplo18/tests/test_exemplo18_home_views.py
@@ -11,3 +11,9 @@ class Exemplo18HomeViewsTestCase(TestCase):
         view = resolve(url)
 
         self.assertEqual(view.func, views.home)
+
+    def test_status_code_200(self):
+        url = reverse('exemplo18:home')
+        respota = self.client.get(url)
+
+        self.assertEqual(respota.status_code, 200)
